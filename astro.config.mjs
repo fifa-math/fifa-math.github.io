41149aa02859d117e9f820443af5882b741fb977
Alexander Mills <alexander.d.mills@gmail.com>
2026-07-23 22:35:07 -0500

    chore: Harden components, implement SEO, and re-sequence claims

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,14 @@ import mdx from '@astrojs/mdx';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 import remarkMermaid from 'remark-mermaidjs';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://fifa-math.github.io',
   integrations: [
     mdx(),
+    sitemap(),
   ],
   vite: {
     plugins: [tailwindcss()]
